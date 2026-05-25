aa4ecc2906ee8268828923f1bf8083323f81cbc3
SakshiTanwar784 <sakshitanwar2604@gmail.com>
2026-05-25 23:21:09 +0530

    Initial project upload

---
+++ sales_queries.sql
@@ -0,0 +1,39 @@
+-- CREATE DATABASE ecommerce_sales;
+
+-- USE ecommerce_sales;
+
+-- SELECT SUM(Sales) AS Total_Sales
+-- FROM sales_data;
+
+-- SELECT Category,
+--        SUM(Sales) AS Total_Sales
+-- FROM sales_data
+-- GROUP BY Category
+-- ORDER BY Total_Sales DESC;
+
+-- SELECT State,
+--        SUM(Profit) AS Total_Profit
+-- FROM sales_data
+-- GROUP BY State
+-- ORDER BY Total_Profit ASC
+-- LIMIT 10;
+
+-- SELECT Discount,
+--        AVG(Profit) AS Avg_Profit
+-- FROM sales_data
+-- GROUP BY Discount
+-- ORDER BY Discount;
+
+-- SELECT Segment,
+--        SUM(Sales) AS Total_Sales
+-- FROM sales_data
+-- GROUP BY Segment
+-- ORDER BY Total_Sales DESC;
+
+-- SELECT `Sub-Category`,
+--        SUM(Profit) AS Total_Profit
+-- FROM sales_data
+-- GROUP BY `Sub-Category`
+-- ORDER BY Total_Profit DESC
+-- LIMIT 10;
+
